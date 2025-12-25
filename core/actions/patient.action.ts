065feddb04798b432436eef987368f4bf2801a5d
Asjiad Farooq <asjadfarooq22@gmail.com>
2025-12-25 22:47:12 +0500

    feat: add core utilities and appwrite config

---
+++ core/actions/patient.action.ts
@@ -0,0 +1,35 @@
+import { ID, Query } from "node-appwrite";
+import { users } from "../appwrite.config";
+
+export interface CreateUserParams {
+  email: string;
+  phone?: string;
+  name?: string;
+}
+
+export const createUser = async (user: CreateUserParams) => {
+  try {
+    // Creating a new user
+    const newUser = await users.create(
+      ID.unique(),
+      user.email,
+      undefined, // Password, if needed, can be set here
+      user.name,
+      user.phone
+    );
+    return newUser;
+  } catch (error: any) {
+    if (error?.code === 409) {
+      // User already exists, retrieve the existing user by email
+      const existingUsers = await users.list([
+        Query.equal("email", user.email),
+      ]);
+
+      if (existingUsers.users && existingUsers.users.length > 0) {
+        return existingUsers.users[0];
+      }
+    } else {
+      throw error; // Re-throw unexpected errors
+    }
+  }
+};
